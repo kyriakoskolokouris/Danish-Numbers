69de2f69981b6c930c2c0c3128735a080e8ab5f2
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-07-08 18:20:35 +0300

    twenty

---
+++ twenty.js
@@ -0,0 +1,97 @@
+const uppers = document.querySelectorAll("upper");
+const down = document.querySelectorAll("down");
+
+// 20
+const twenties = document.getElementById("twenties");
+const twenty = document.getElementById("twenty");
+
+twenties.addEventListener("click", (e) => {
+    twenty.style.display === "block" ? twenty.style.display = "none"
+     : twenty.style.display = "block"
+})
+// 30
+const thirties = document.getElementById("thirties");
+const thirty = document.getElementById("thirty");
+
+thirties.addEventListener("click", () => {
+    thirty.style.display === "block"
+     ? thirty.style.display = "none"
+     : thirty.style.display = "block"
+})
+// 40
+const forties = document.getElementById("forties");
+const forty = document.getElementById("forty");
+
+
+forties.addEventListener("click", () => {
+    forty.style.display === "block"
+     ? forty.style.display = "none"
+     : forty.style.display = "block"
+})
+
+// 50
+const fifties = document.getElementById("fifties");
+const fifty = document.getElementById("fifty");
+
+
+fifties.addEventListener("click", () => {
+    fifty.style.display === "block"
+     ? fifty.style.display = "none"
+     : fifty.style.display = "block"
+})
+
+// 60
+const sixties = document.getElementById("sixties");
+const sixty = document.getElementById("sixty");
+
+sixties.addEventListener("click", () => {
+    sixty.style.display === "block"
+     ? sixty.style.display = "none"
+     : sixty.style.display = "block"
+})
+// 70
+const seventies = document.getElementById("seventies");
+const seventy = document.getElementById("seventy");
+
+seventies.addEventListener("click", () => {
+    seventy.style.display === "block"
+     ? seventy.style.display = "none"
+     : seventy.style.display = "block"
+})
+// 80
+const eighties = document.getElementById("eighties");
+const eighty = document.getElementById("eighty");
+
+eighties.addEventListener("click", () => {
+    eighty.style.display === "block"
+     ? eighty.style.display = "none"
+     : eighty.style.display = "block"
+})
+// 90
+const ninties = document.getElementById("ninties");
+const ninety = document.getElementById("ninety");
+
+ninties.addEventListener("click", () => {
+    ninety.style.display === "block"
+     ? ninety.style.display = "none"
+     : ninety.style.display = "block"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
