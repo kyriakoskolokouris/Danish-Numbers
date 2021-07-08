6adedf2a676b301eac4aac4a4a83b84c6f08108a
kyriakos.kolokouris <kyriakoskolokouris@gmail.com>
2021-07-08 17:04:47 +0300

    first commit

---
+++ card.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll('.number-card');
+const audio = document.querySelectorAll(".audio");
+
+
+
+
+function flipCard() {
+  this.classList.toggle('flip');
+}
+
+
+cards.forEach(card => card.addEventListener('click', flipCard,));
+
